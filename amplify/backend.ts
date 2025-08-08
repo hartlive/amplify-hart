029d1f34dfff2b52a7a57be9e53b2724d9efaeda
Cason <l_jiaming27@163.com>
2025-08-08 16:31:27 +0800

    [buckets] 指定buckets

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,36 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { storage } from './storage/resource';
 
 defineBackend({
   auth,
-  data,
-  storage
+  data
 });
+
+backend.addOutput({
+  storage: {
+    aws_region: 'eu-central-1',
+    bucket_name: 'hart-eu',
+
+    buckets: [
+      {
+        aws_region: 'eu-central-1',
+        bucket_name: 'hart-eu',
+        name: 'hart-eu',
+        paths: {
+          'public/*': {
+            guest: ['read', 'write'],
+            authenticated: ['read', 'write'],
+          },
+          'protected/{entity_id}/*': {
+            authenticated: ['read', 'write'],
+            entityidentity: ['read', 'write']
+          },
+          'private/{entity_id}/*': {
+            entityidentity: ['read', 'write']
+          }
+        }
+      }
+    ]
+  }
+})
